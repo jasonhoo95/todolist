e6688f91d0fbe79bf04bfd371af93d94b676fe54
jasonhoo95 <hoochongyi95@gmail.com>
2022-09-24 23:09:00 +0800

    adding synrcuhous tag

--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,11 @@ export default function Document() {
 				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
-				<script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js" async></script>
+				<script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js" defer></script>
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 				<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
-				<script src="https://unpkg.com/@popperjs/core@2"></script>
+				<script src="https://unpkg.com/@popperjs/core@2" defer></script>
 			</Head>
 			<body>
 				<Main />
